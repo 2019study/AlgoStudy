d3189854cd2851a63ef3959e4d91a739e87c9e66
201502029 <tnehd158@naver.com>
2020-01-04 16:03:51 +0900

    문제 프로젝트 생성.

---
+++ 김수연/Algo_2309/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // 2309번 일곱 난쟁이 문제
+
+    }
+}
---
+++ 김수연/Algo_9267/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // 9267번 A + B 문제
+        
+    }
+}
