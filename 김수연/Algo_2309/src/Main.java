8cde1d32a9c3155430890aa2c9b51ddb8da984a6
201502029 <tnehd158@naver.com>
2020-01-06 12:10:49 +0900

    2주차 2309번 일곱 난쟁이 문제

--- 김수연/Algo_2309/src/Main.java
+++ 김수연/Algo_2309/src/Main.java
@@ -1,7 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // 2309번 일곱 난쟁이 문제
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        int totalSum = 0;
 
+        for (int i = 0; i < 9; i++) {
+            int height = scan.nextInt();
+            arr.add(height);
+            totalSum += height;
+        }
+
+        Collections.sort(arr);
+
+        loop:
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (totalSum == 100 + arr.get(i) + arr.get(j)) {
+                    arr.remove(i);
+                    arr.remove(j - 1);
+                    break loop;
+                }
+            }
+        }
+
+        for (int i = 0; i < 7; i++)
+            System.out.println(arr.get(i));
     }
 }
