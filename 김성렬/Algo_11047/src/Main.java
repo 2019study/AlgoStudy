b351e9cf74ac54ee7f9a72a60d5f8d1753c5e146
SteveKimSR <kimsr96@naver.com>
2020-01-07 08:40:58 +0900

    update 20.01.07

--- 김성렬/Algo_11047/src/Main.java
+++ 김성렬/Algo_11047/src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int[] kind = new int[N];
+        int K = input.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < N; i++)
+            kind[i] = input.nextInt();
+
+        for (int i = N - 1; i >= 0; i--) {
+            if(K / kind[i] > 0) {
+                count += (K / kind[i]);
+                K=K%kind[i];
+            }
+        }
+        System.out.print(count);
     }
 }
--- 김성렬/Algo_2309/src/Main.java
+++ 김성렬/Algo_2309/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> dwarf = new ArrayList<Integer>();
+        for (int i = 0; i < 9; i++)
+            dwarf.add(sc.nextInt());
+        Collections.sort(dwarf);
+        int sum = 0;
+        for (int i = 0; i < 9; i++)
+            sum += dwarf.get(i);
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if ((sum - (dwarf.get(i) + dwarf.get(j))) == 100) {
+                    int temp = dwarf.get(i);
+                    int temp2 = dwarf.get(j);
+                    dwarf.remove((Object)temp);
+                    dwarf.remove((Object)temp2);
+                    for (int k = 0; k < 7; k++) {
+                        System.out.println(dwarf.get(k));
+                    }
+                    return;
+                }
+            }
+        }
 
     }
 }
