60dfca8341cf746b361eeca86c5fc4a31b884ede
201502029 <tnehd158@naver.com>
2020-01-07 14:31:38 +0900

    2주차 11057번 동전 0 문제

--- 김수연/Algo_11047/src/Main.java
+++ 김수연/Algo_11047/src/Main.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // 11047번 동전 0 문제
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int K = scan.nextInt();
+
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        for (int i = 0; i < N; i++) {
+            int value = scan.nextInt();
+            arr.add(value);
+        }
+
+        N--;
+        int count = 0;
+        while (K != 0) {
+            if (arr.get(N) > K)
+                N--;
+            else {
+                while (K >= arr.get(N)) {
+                    K = K - arr.get(N);
+                    count = count + 1;
+                }
+            }
+        }
+
+        System.out.println(count);
     }
 }
