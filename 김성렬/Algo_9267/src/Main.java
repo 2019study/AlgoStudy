3ba85ca9661ed7557f14e95d3ae6d39b14e27d8a
SteveKimSR <kimsr96@naver.com>
2020-01-11 12:55:00 +0900

    just commit 20.01.11

--- 김성렬/Algo_9267/src/Main.java
+++ 김성렬/Algo_9267/src/Main.java
@@ -1,46 +1,16 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double a = input.nextDouble();
-        double b = input.nextDouble();
-        double sum = input.nextDouble();
+    static Scanner input = new Scanner(System.in);
+    static double a = input.nextDouble();
+    static double b = input.nextDouble();
 
-        if (checkOK(sum, a, b))
-            System.out.print("YES");
-        else
-            System.out.print("NO");
+    public static void main(String[] args) {
+        double sum = input.nextDouble();
+        check(sum);
     }
 
-    public static boolean checkOK(double sum, double a, double b) {
-        if (sum != 0) {
-            if (a == 0 && b == 0)
-                return false;
-            else {
-                if (a == 0) {
-                    if (sum % b == 0)
-                        return true;
-                    else
-                        return false;
-                }
-                if (b == 0) {
-                    if (sum % a == 0)
-                        return true;
-                    else
-                        return false;
-                }
-            }
-            if ((sum % a == 0) || (sum % b == 0) || (sum % (a + b) == 0))
-                return true;
-            double time = (int) (sum / (a + b));
-            for (double i = 1; i <= time; i++) {
-                double tempsum = sum - (a + b) * i;
-                if ((tempsum % a == 0) || (tempsum % b == 0) || (tempsum % (a + b) == 0))
-                    return true;
-            }
-        }else if(a == 0 || b == 0)
-            return true;
-        return false;
+    public static boolean check(double sum) {
     }
 }
