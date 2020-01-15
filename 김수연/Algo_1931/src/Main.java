7482a2e2e5b225b6bab3808801c338686cacf521
201502029 <tnehd158@naver.com>
2020-01-15 10:01:22 +0900

    3주차 2252번 줄 세우기 문제

--- 김수연/Algo_1931/src/Main.java
+++ 김수연/Algo_1931/src/Main.java
@@ -1,8 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // 1931번 회의실배정 문제
 
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int[][] list = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            int sTime = scan.nextInt();
+            int eTime = scan.nextInt();
+
+            list[i][0] = sTime;
+            list[i][1] = eTime;
+        }
+
+        Arrays.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        for (int i = 0; i < n; i++)
+            System.out.println("list: " + list[i][0] + " " + list[i][1]);
     }
 }
--- 김수연/Algo_2252/src/Main.java
+++ 김수연/Algo_2252/src/Main.java
@@ -1,8 +1,45 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // 2252번 줄 세우기 문제
+        Scanner scan = new Scanner(System.in);
 
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+
+        ArrayList<Integer>[] list = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++)
+            list[i] = new ArrayList<Integer>();
+        int[] topology = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            int value1 = scan.nextInt();
+            int value2 = scan.nextInt();
+
+            list[value1].add(value2);
+            topology[value2]++;
+        }
+
+        Queue<Integer> answer = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (topology[i] == 0)
+                answer.add(i);
+        }
+
+        while (!answer.isEmpty()) {
+            int temp = answer.poll();
+            System.out.print(temp + " ");
+
+            for (int i = 0; i < list[temp].size(); i++) {
+                int temp2 = list[temp].get(i);
+                topology[temp2]--;
+                if (topology[temp2] == 0)
+                    answer.add(temp2);
+            }
+        }
     }
 }
